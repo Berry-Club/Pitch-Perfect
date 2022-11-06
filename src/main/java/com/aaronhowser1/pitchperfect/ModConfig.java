d306f0f601e7e2ebdd588bd335b3a778c35f97bb
Aaron Howser <aaronhowser1@gmail.com>
2022-11-06 08:47:12 -0600

    Revert "Update to 1.18"
    This reverts commit 3acf781068d897a44226d7e620848d788c13ac9b.

---
+++ src/main/java/com/aaronhowser1/pitchperfect/ModConfig.java
@@ -0,0 +1,51 @@
+package com.aaronhowser1.pitchperfect;
+
+import com.electronwill.nightconfig.core.file.CommentedFileConfig;
+import com.electronwill.nightconfig.core.io.WritingMode;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.fml.common.Mod;
+
+import java.nio.file.Path;
+
+@Mod.EventBusSubscriber
+@SuppressWarnings("WeakerAccess")
+public class ModConfig {
+    public static final String CATEGORY_GENERAL = "general";
+
+    private static final ForgeConfigSpec.Builder COMMON_BUILDER = new ForgeConfigSpec.Builder();
+
+    public static ForgeConfigSpec COMMON_CONFIG;
+
+    public static ForgeConfigSpec.BooleanValue DEBUG_PITCH;
+
+    public static ForgeConfigSpec.IntValue INSTRUMENT_VOLUME;
+
+    static {
+
+        COMMON_BUILDER.comment("General settings").push(CATEGORY_GENERAL);
+
+        DEBUG_PITCH = COMMON_BUILDER
+                .comment("\nDisplay in console the pitch?")
+                .define("debug_pitch", false);
+
+        INSTRUMENT_VOLUME = COMMON_BUILDER
+                .comment("\nThe volume of instruments")
+                .defineInRange("instrument_volume",1, 0, Integer.MAX_VALUE);
+
+        COMMON_BUILDER.pop();
+
+        COMMON_CONFIG = COMMON_BUILDER.build();
+    }
+
+    public static void loadConfig(ForgeConfigSpec spec, Path path) {
+
+        final CommentedFileConfig configData = CommentedFileConfig.builder(path)
+                .sync()
+                .autosave()
+                .writingMode(WritingMode.REPLACE)
+                .build();
+
+        configData.load();
+        spec.setConfig(configData);
+    }
+}
