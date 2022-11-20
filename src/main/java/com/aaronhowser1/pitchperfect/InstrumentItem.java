a4edca7642ad5964aa31deaeadae23fdb2b6a81e
Aaron Howser <aaronhowser1@gmail.com>
2022-11-20 03:48:24 -0600

    Attacks now deal damage

--- src/main/java/com/aaronhowser1/pitchperfect/InstrumentItem.java
+++ src/main/java/com/aaronhowser1/pitchperfect/InstrumentItem.java
@@ -1,5 +1,8 @@
 package com.aaronhowser1.pitchperfect;
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.ImmutableSetMultimap;
+import com.google.common.collect.Multimap;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundSource;
@@ -7,21 +10,30 @@ import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.common.util.Lazy;
+
+import java.util.Map;
 
 public class InstrumentItem extends Item {
 
     public final SoundEvent sound;
+    private final Lazy<ImmutableSetMultimap<Attribute, AttributeModifier>> attributeModifiers;
 
     public InstrumentItem(SoundEvent s) {
         super(new Item.Properties()
                 .stacksTo(1)
                 .tab(ModCreativeModeTab.MOD_TAB)
         );
-        sound = s;
+        this.sound = s;
+        attributeModifiers = buildDefaultAttributes();
     }
 
     @Override
@@ -30,7 +42,7 @@ public class InstrumentItem extends Item {
         float pitch = player.getXRot();
         pitch = map(pitch, -90,90,2,0.5F); //from [-90,90] to [2,0.5], high->low bc big number = low pitch
         playSound(level, pitch, player.getX(), player.getY(), player.getZ());
-        spawnNote(level, pitch, player.getX(), player.getY(), player.getZ());
+        spawnNote(level, pitch, player.getX()+player.getLookAngle().x(), player.getEyeY()+player.getLookAngle().y(), player.getZ()+player.getLookAngle().z());
         return InteractionResultHolder.fail(itemStack);
     }
 
@@ -106,5 +118,38 @@ public class InstrumentItem extends Item {
                     Math.max(0.0F, Mth.sin((pitch + 0.33333334F) * ((float) Math.PI * 2F)) * 0.65F + 0.35F);
             default -> 0;
         };
+    }
+
+    //Ok beyond this, I barely understood
+    //Mostly stolen from https://github.com/Tslat/Advent-Of-Ascension/blob/1.18.2/source/content/item/weapon/maul/BaseMaul.java (with permission, thanks Tslat)
+    public float getAttackDamage() {
+        return 0.01F;
+    }
+
+    //Lazy is apparently a map, of type ImmutableSetMultimap, which maps attributes to how they're being modified
+    //Like, damage to +2, or whatever
+    protected Lazy<ImmutableSetMultimap<Attribute, AttributeModifier>> buildDefaultAttributes() {
+        return Lazy.of(() -> ImmutableSetMultimap.of(
+                Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Weapon modifier", getAttackDamage(), AttributeModifier.Operation.ADDITION)
+        ));
+    }
+    @Override
+    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlot slot, ItemStack stack) {
+        //If and only if item is in the hand, for some reason
+        if (slot == EquipmentSlot.MAINHAND) {
+            //Make a new map
+            Multimap<Attribute, AttributeModifier> newMap = HashMultimap.create();
+            //Get the existing one, made by the Lazy constructor
+            ImmutableSetMultimap<Attribute, AttributeModifier> attributes = this.attributeModifiers.get();
+            //GET EVERY VALUE OF THE ORIGINAL and put it in the new one
+            for (Map.Entry<Attribute, AttributeModifier> entry : attributes.entries()) {
+                newMap.put(entry.getKey(), entry.getValue());
+            }
+            //Return the new one
+            return newMap;
+            //This is so fucking stupid
+        }
+
+        return super.getAttributeModifiers(slot, stack);
     }
 }
