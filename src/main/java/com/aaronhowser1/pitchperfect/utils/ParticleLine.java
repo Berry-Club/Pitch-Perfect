4d2f9f76e7f361b7fbe928ca9d1aaa17c8035939
Aaron Howser <aaronhowser1@gmail.com>
2022-11-27 15:11:06 -0600

    ??????????

--- src/main/java/com/aaronhowser1/pitchperfect/utils/ParticleLine.java
+++ src/main/java/com/aaronhowser1/pitchperfect/utils/ParticleLine.java
@@ -3,24 +3,23 @@ package com.aaronhowser1.pitchperfect.utils;
 
 import com.aaronhowser1.pitchperfect.config.CommonConfigs;
 import net.minecraft.Util;
-import net.minecraft.core.particles.ParticleType;
-import net.minecraft.core.particles.ParticleTypes;
+
 import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.world.phys.Vec3;
 
 //Probably a really bad way to do this but I am sleep deprived and out of ideas
 public class ParticleLine {
 
-    private Vec3 originVec;
-    private Vec3 destinationVec;
-    private SimpleParticleType particleType;
+    private final Vec3 originPositionVec;
+    private final Vec3 destinationPositionVec;
+    private final SimpleParticleType particleType;
     private int iteration;
-    private int particlesPerBlock;
-    private long totalTravelTime;
+    private final int particlesPerBlock;
+    private final long totalTravelTime;
 
-    public ParticleLine(Vec3 originVec, Vec3 destinationVec, SimpleParticleType particleType) {
-        this.originVec = originVec;
-        this.destinationVec = destinationVec;
+    public ParticleLine(Vec3 originPositionVec, Vec3 destinationPositionVec, SimpleParticleType particleType) {
+        this.originPositionVec = originPositionVec;
+        this.destinationPositionVec = destinationPositionVec;
         this.particleType = particleType;
         this.iteration = 1;
         this.particlesPerBlock = 9;
@@ -28,23 +27,26 @@ public class ParticleLine {
     }
 
     public void spawnLine() {
-        Vec3 pathVector = originVec.vectorTo(destinationVec);
+        Vec3 pathVector = originPositionVec.vectorTo(destinationPositionVec);
         float pathSize = (float) pathVector.length();
         float distanceBetween = pathSize/particlesPerBlock;
         int totalParticleCount = (int) (pathSize * (float) particlesPerBlock);
         Vec3 pathUnitVector = pathVector.normalize();
+        Vec3 pathDeltaVector = new Vec3(
+                pathUnitVector.x()*distanceBetween,
+                pathUnitVector.y()*distanceBetween,
+                pathUnitVector.z()*distanceBetween
+        );
         long timePerParticle = totalTravelTime/totalParticleCount;
 
-        spawnNextParticle(totalParticleCount, pathUnitVector, originVec, distanceBetween, timePerParticle);
+        System.out.println("Path Size:\n"+pathSize+"\nDistance Between:\n"+distanceBetween+"\nParticle Count:\n"+totalParticleCount);
+
+        spawnNextParticle(totalParticleCount, pathDeltaVector, originPositionVec, distanceBetween, timePerParticle);
     }
 
     public void spawnNextParticle(int totalParticleCount, Vec3 deltaVector, Vec3 newOriginVec, float distanceBetween, long timePerParticle) {
         if (iteration <= totalParticleCount) {
-            Vec3 particlePositionVector = newOriginVec.add(
-                    deltaVector.x()*distanceBetween,
-                    deltaVector.y()*distanceBetween,
-                    deltaVector.z()*distanceBetween
-            );
+            Vec3 particlePositionVector = newOriginVec.add(deltaVector);
 
             ClientUtils.spawnParticle(
                     particleType,
@@ -65,25 +67,5 @@ public class ParticleLine {
             });
         }
     }
-
-//    private void spawnNextParticle(int iteration, double x, double y, double z, long waitTime, int totalParticleCount) {
-//        if (iteration <= totalParticleCount) {
-//            Vec3 particleVector = origin.add(
-//                    pathUnitVector.x()*distanceBetween*iteration,
-//                    pathUnitVector.y()*distanceBetween*iteration,
-//                    pathUnitVector.z()*distanceBetween*iteration
-//            );
-//            double particleX = particleVector.x();
-//            double particleY = particleVector.y();
-//            double particleZ = particleVector.z();
-//        }
-//        ClientUtils.spawnParticle(ParticleTypes.ANGRY_VILLAGER,z,y,x,1,1,1);
-//        Util.backgroundExecutor().submit( () -> {
-//            try {
-//                Thread.sleep(waitTime);
-//            } catch (Exception ignored) {
-//            }
-//            spawnNextParticle(origin,destination,iteration+1);
-//        };
 
 }
