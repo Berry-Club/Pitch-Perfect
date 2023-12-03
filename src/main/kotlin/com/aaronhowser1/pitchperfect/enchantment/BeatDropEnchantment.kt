fa7a667d120b72835cc00ba8ed409eb8606c40a1
Aaron Howser <aaronhowser1@gmail.com>
2023-12-03 08:25:04 -0600

    Feature parity

--- src/main/kotlin/com/aaronhowser1/pitchperfect/enchantment/BeatDropEnchantment.kt
+++ src/main/kotlin/com/aaronhowser1/pitchperfect/enchantment/BeatDropEnchantment.kt
@@ -1,6 +1,6 @@
-package com.aaronhowser1.pitchperfect.enchantment;
+package com.aaronhowser1.pitchperfect.enchantment
 
-public class BeatDropEnchantment {
+object BeatDropEnchantment {
     //On receiving fall damage, emit a wave of notes in a circle, knocking back mobs
     //When you fall far enough to cause fall damage, start emitting notes that rise in pitch
     //Higher height = more knockback + range
--- src/main/kotlin/com/aaronhowser1/pitchperfect/enchantment/UnnamedDrStrangeEnchant.kt
+++ src/main/kotlin/com/aaronhowser1/pitchperfect/enchantment/UnnamedDrStrangeEnchant.kt
@@ -1,6 +1,6 @@
-package com.aaronhowser1.pitchperfect.enchantment;
+package com.aaronhowser1.pitchperfect.enchantment
 
-public class UnnamedDrStrangeEnchant {
+object UnnamedDrStrangeEnchant {
     //https://www.youtube.com/watch?t=248&v=kwFvUBI12Ik&feature=youtu.be
     //Would probably require an entity
 }
