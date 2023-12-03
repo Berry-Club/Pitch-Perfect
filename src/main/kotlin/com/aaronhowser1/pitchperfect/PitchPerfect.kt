b57a55c2a97b32a452792c6a2f8af076ff986cfe
Aaron Howser <aaronhowser1@gmail.com>
2023-12-03 04:59:08 -0600

    Progress with items

--- src/main/kotlin/com/aaronhowser1/pitchperfect/PitchPerfect.kt
+++ src/main/kotlin/com/aaronhowser1/pitchperfect/PitchPerfect.kt
@@ -3,9 +3,12 @@ package com.aaronhowser1.pitchperfect
 import com.aaronhowser1.pitchperfect.config.ClientConfig
 import com.aaronhowser1.pitchperfect.config.CommonConfig
 import com.aaronhowser1.pitchperfect.config.ServerConfig
+import com.aaronhowser1.pitchperfect.item.ModItems
+import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.fml.ModLoadingContext
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.config.ModConfig
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext
 import org.apache.logging.log4j.Level
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
@@ -24,5 +27,16 @@ object PitchPerfect {
             registerConfig(ModConfig.Type.COMMON, CommonConfig.SPEC, "pitchperfect-common.toml")
             registerConfig(ModConfig.Type.SERVER, ServerConfig.SPEC, "pitchperfect-server.toml")
         }
+
+        val eventBus = FMLJavaModLoadingContext.get().modEventBus
+
+        ModItems.register(eventBus)
+//        ModEnchantments.register(eventBus)
+//        ModSounds.register(eventBus)
+//
+//        ModPacketHandler.setup()
+
+        MinecraftForge.EVENT_BUS.register(this)
+
     }
 }
