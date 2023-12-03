10d8b66e9fa0b318d366fd53764f1a2eae8b737d
Aaron Howser <aaronhowser1@gmail.com>
2023-12-03 03:22:29 -0600

    Removed the java classes

--- src/main/java--/com/aaronhowser1/pitchperfect/enchantment/BeatDropEnchantment.java
+++
@@ -1,7 +0,0 @@
-package com.aaronhowser1.pitchperfect.enchantment;
-
-public class BeatDropEnchantment {
-    //On receiving fall damage, emit a wave of notes in a circle, knocking back mobs
-    //When you fall far enough to cause fall damage, start emitting notes that rise in pitch
-    //Higher height = more knockback + range
-}
