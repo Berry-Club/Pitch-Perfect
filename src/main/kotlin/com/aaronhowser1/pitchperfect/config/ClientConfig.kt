e2e18c57883edf7017f9992adac68e65481b5215
Aaron Howser <aaronhowser1@gmail.com>
2023-12-06 05:29:30 -0600

    Remove unused config

--- src/main/kotlin/com/aaronhowser1/pitchperfect/config/ClientConfig.kt
+++ src/main/kotlin/com/aaronhowser1/pitchperfect/config/ClientConfig.kt
@@ -8,7 +8,6 @@ object ClientConfig {
 
     val SPEC: ForgeConfigSpec
     val VOLUME: ForgeConfigSpec.ConfigValue<Double>
-    val MIN_ATTACK_VOLUME: ForgeConfigSpec.ConfigValue<Double>
     val ELECTRIC_PARTICLE_DENSITY: ForgeConfigSpec.ConfigValue<Int>
     val ELECTRIC_PARTICLE_ISWAVE: ForgeConfigSpec.ConfigValue<Boolean>
 
@@ -18,10 +17,6 @@ object ClientConfig {
         VOLUME = BUILDER
             .comment(" The volume that instruments play at, when right-clicked.")
             .defineInRange("Instrument Volume", 0.5, 0.0, 3.0)
-
-        MIN_ATTACK_VOLUME = BUILDER
-            .comment(" The minimum volume each individual note plays at, when attacking.\n Uses the equation:\n  Math.max( (1.5*instrumentVolume / amountOfParticles), minimumAttackVolume)")
-            .defineInRange("Attack Volume", 0.5, 0.0, 3.0)
 
         ELECTRIC_PARTICLE_DENSITY = BUILDER
             .comment(" How many particles per block should be spawned along the lightning path.\n There's a minimum of 1 tick per particle, so higher numbers may cause the particles to take longer than the jump time.")
