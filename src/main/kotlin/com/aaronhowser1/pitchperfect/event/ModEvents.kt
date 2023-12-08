e8d7569d4475f68ce3f8c46779aab952d70c869e
Aaron Howser <aaronhowser1@gmail.com>
2023-12-08 04:09:33 -0600

    Trying to improve the onLivingSpawn thing

--- src/main/kotlin/com/aaronhowser1/pitchperfect/event/ModEvents.kt
+++ src/main/kotlin/com/aaronhowser1/pitchperfect/event/ModEvents.kt
@@ -5,17 +5,13 @@ import com.aaronhowser1.pitchperfect.config.ServerConfig
 import com.aaronhowser1.pitchperfect.enchantment.AndHisMusicWasElectricEnchantment
 import com.aaronhowser1.pitchperfect.item.InstrumentItem
 import com.aaronhowser1.pitchperfect.item.ModItems
-import com.aaronhowser1.pitchperfect.song.SongRegistry
 import com.aaronhowser1.pitchperfect.utils.CommonUtils.isMonster
-import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.InteractionHand
 import net.minecraft.world.entity.LivingEntity
-import net.minecraft.world.level.block.NoteBlock
 import net.minecraftforge.event.TickEvent
 import net.minecraftforge.event.TickEvent.ServerTickEvent
 import net.minecraftforge.event.entity.living.LivingHurtEvent
 import net.minecraftforge.event.entity.living.LivingSpawnEvent
-import net.minecraftforge.event.level.BlockEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod
 import kotlin.random.Random
@@ -62,11 +58,12 @@ object ModEvents {
     @SubscribeEvent
     fun onLivingSpawn(event: LivingSpawnEvent) {
         val entity = event.entity
-        if (
-            !entity.isMonster() ||
-            !entity.mainHandItem.isEmpty ||
-            ServerConfig.MOB_SPAWNS_WITH_INSTRUMENT_CHANCE.get() < Random.nextFloat()
-        ) return
+
+        if (!entity.isMonster()) return
+        if (!entity.mainHandItem.isEmpty) return
+
+        val chance = ServerConfig.MOB_SPAWNS_WITH_INSTRUMENT_CHANCE.get()
+        if (Random.nextDouble() > chance) return
 
         val randomInstrument = ModItems.ITEM_REGISTRY.entries.random().get()
         entity.setItemInHand(InteractionHand.MAIN_HAND, randomInstrument.defaultInstance)
