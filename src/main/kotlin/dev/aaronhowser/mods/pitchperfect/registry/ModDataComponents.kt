062a90d4ba6b7347e680418f8ed7120e8783e6f2
Aaron Howser <aaronhowser1@gmail.com>
2024-07-06 19:06:08 -0500

    BooleanItemComponent

--- src/main/kotlin/dev/aaronhowser/mods/pitchperfect/registry/ModDataComponents.kt
+++ src/main/kotlin/dev/aaronhowser/mods/pitchperfect/registry/ModDataComponents.kt
@@ -1,6 +1,7 @@
 package dev.aaronhowser.mods.pitchperfect.registry
 
 import dev.aaronhowser.mods.pitchperfect.PitchPerfect
+import dev.aaronhowser.mods.pitchperfect.item.component.BooleanItemComponent
 import dev.aaronhowser.mods.pitchperfect.item.component.InstrumentComponent
 import dev.aaronhowser.mods.pitchperfect.item.component.MusicItemComponent
 import net.minecraft.core.component.DataComponentType
@@ -20,6 +21,11 @@ object ModDataComponents {
     val MUSIC_COMPONENT: DeferredHolder<DataComponentType<*>, DataComponentType<MusicItemComponent>> =
         DATA_COMPONENT_REGISTRY.registerComponentType("music") {
             it.persistent(MusicItemComponent.CODEC).networkSynchronized(MusicItemComponent.STREAM_CODEC)
+        }
+
+    val IS_RECORDING_COMPONENT: DeferredHolder<DataComponentType<*>, DataComponentType<BooleanItemComponent>> =
+        DATA_COMPONENT_REGISTRY.registerComponentType("is_recording") {
+            it.persistent(BooleanItemComponent.CODEC).networkSynchronized(BooleanItemComponent.STREAM_CODEC)
         }
 
 }
