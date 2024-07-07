dfe43dfd2ebe13f31f5442af1e92affb9b403a25
Aaron Howser <aaronhowser1@gmail.com>
2024-07-07 03:46:09 -0500

    refactor config

--- src/main/kotlin/dev/aaronhowser/mods/pitchperfect/config/ServerConfig.kt
+++ src/main/kotlin/dev/aaronhowser/mods/pitchperfect/config/ServerConfig.kt
@@ -43,50 +43,50 @@ class ServerConfig(
         builder.push("Server")
 
         CHANCE_MOB_SPAWNS_WITH_INSTRUMENT = builder
-            .comment(" The chance that a monster with an empty main-hand will have it filled with an instrument on spawn.")
+            .comment("The chance that a monster with an empty main-hand will have it filled with an instrument on spawn.")
             .defineInRange("Mob Spawns With Instrument Chance", 0.005, 0.0, 1.0)
 
         HEAL_RANGE = builder
-            .comment(" The range in blocks around the user that the Healing Beat enchantment checks for mobs to heal.")
+            .comment("The range in blocks around the user that the Healing Beat enchantment checks for mobs to heal.")
             .defineInRange("Healing Beat Range", 5.0, 0.0, 64.0)
         HEAL_AMOUNT = builder
-            .comment(" How much health is healed each time Healing Beat is used.")
+            .comment("How much health is healed each time Healing Beat is used.")
             .defineInRange("Healing Beat Amount", 0.25, 0.0, Double.MAX_VALUE)
         HEAL_COOLDOWN_PER = builder
-            .comment(" How many ticks to cool down for every mob healed.\n Example: healing 2 mobs has a default cooldown of 3 ticks.")
+            .comment("How many ticks to cool down for every mob healed.\nExample: healing 2 mobs has a default cooldown of 3 ticks.")
             .defineInRange("Healing Cooldown Multiplier", 1.5, 0.0, Double.MAX_VALUE)
 
         //TODO: Move this to use entity type tags
         HEALING_BEAT_WHITELIST = builder
-            .comment(" Mobs that Healing Beat can always heal\n Example: [\"minecraft:piglin\"]")
+            .comment("Mobs that Healing Beat can always heal\nExample: [\"minecraft:piglin\"]")
             .defineListAllowEmpty("Healing Beat Whitelist", listOf()) { true }
 
         //TODO: Move this to use entity type tags
         HEALING_BEAT_BLACKLIST = builder
-            .comment(" Mobs that Healing Beat can never heal\n Example: [\"minecraft:cow\"]")
+            .comment("Mobs that Healing Beat can never heal\nExample: [\"minecraft:cow\"]")
             .defineListAllowEmpty("Healing Beat Blacklist", listOf()) { true }
 
         BWAAAP_RANGE = builder
-            .comment(" The reach the BWAAAP enchantment has.")
+            .comment("The reach the BWAAAP enchantment has.")
             .defineInRange("BWAAAP Range", 5.0, 0.0, 128.0)
         BWAAAP_STRENGTH = builder
-            .comment(" The strength the BWAAAP enchantment has. Decreases with distance.\n Uses the equation:\n  strength * targetsDistanceToMaxRange")
+            .comment("The strength the BWAAAP enchantment has. Decreases with distance.\nUses the equation:\nstrength * targetsDistanceToMaxRange")
             .defineInRange("BWAAAP Strength", 1.25, 0.0, Double.MAX_VALUE)
         BWAAAP_COOLDOWN_FACTOR = builder
-            .comment(" How many ticks to cool down for every mob BWAAAP'd.")
+            .comment("How many ticks to cool down for every mob BWAAAP'd.")
             .defineInRange("BWAAAP Cooldown Multiplier", 10.0, 0.0, Double.MAX_VALUE)
 
         ELECTRIC_RANGE = builder
-            .comment(" The range in blocks around the attacked mob that should be effected by the \"And His Music Was Electric\" enchantment.")
+            .comment("The range in blocks around the attacked mob that should be effected by the \"And His Music Was Electric\"enchantment.")
             .defineInRange("Electric Range", 5.0, 0.0, 128.0)
         ELECTRIC_DAMAGE_FACTOR = builder
-            .comment(" The rate at which the damage decreases with each jump.\n Uses the equation:\n  damage = originalDamage * (damageFactor ^ jumpNumber)")
+            .comment("The rate at which the damage decreases with each jump.\nUses the equation:\ndamage = originalDamage * (damageFactor ^ jumpNumber)")
             .defineInRange("Electric Damage Multiplier", 0.75, 0.0, Double.MAX_VALUE)
         ELECTRIC_JUMP_TIME = builder
-            .comment(" How many ticks before the lightning jumps to the next entity.")
+            .comment("How many ticks before the lightning jumps to the next entity.")
             .defineInRange("Electric Jump Time", 4, 0, Integer.MAX_VALUE)
         ELECTRIC_MAX_JUMPS = builder
-            .comment(" How many times the lightning can jump.\n If it would do less than 0.5 damage, it will stop.")
+            .comment("How many times the lightning can jump.\nIf it would do less than 0.5 damage, it will stop.")
             .defineInRange("Electric Jump Limit", Integer.MAX_VALUE, 0, Integer.MAX_VALUE)
 
         builder.pop()
