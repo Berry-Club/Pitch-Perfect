bd2ba80631da432eeaf14fce004487a3f77d0004
Aaron Howser <aaronhowser1@gmail.com>
2024-07-07 04:57:04 -0500

    Progress

--- src/main/kotlin/dev/aaronhowser/mods/pitchperfect/song/SongBuilder.kt
+++ src/main/kotlin/dev/aaronhowser/mods/pitchperfect/song/SongBuilder.kt
@@ -4,44 +4,31 @@ import dev.aaronhowser.mods.pitchperfect.item.component.SongItemComponent
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument
 
 class SongBuilder(
-    val startingTick: Long
+    private val startingTick: Long
 ) {
 
-    private val tickToNotesMap: MutableMap<Long, MutableMap<NoteBlockInstrument, List<Float>>> = mutableMapOf()
+    private val notesMap: MutableMap<NoteBlockInstrument, Map<Int, List<Float>>> = mutableMapOf()
 
     fun addNote(
         currentTick: Long,
         instrument: NoteBlockInstrument,
         pitch: Float
     ) {
-        val currentTickMap = tickToNotesMap.getOrDefault(currentTick, mutableMapOf())
-        val currentPitches = currentTickMap.getOrDefault(instrument, mutableListOf())
+        val ticksSinceStart = (currentTick - startingTick).toInt()
 
-        val newPitches = currentPitches + pitch
-        currentTickMap[instrument] = newPitches
+        val previousInstrumentBeats = notesMap[instrument] ?: emptyMap()
+        val previousBeatsThisTick = previousInstrumentBeats[ticksSinceStart] ?: emptyList()
+        val withThisPitch = previousBeatsThisTick + pitch
+        val newInstrumentBeats = previousInstrumentBeats + (ticksSinceStart to withThisPitch)
 
-        tickToNotesMap[currentTick] = currentTickMap   // Is this one required?
+        notesMap[instrument] = newInstrumentBeats
     }
 
     fun build(): SongItemComponent {
-        var previousTick = startingTick
 
-        val beats = mutableListOf<SongItemComponent.SoundsWithDelayAfter>()
+        println(notesMap.toString())
 
-        for ((tick, tickNotes) in tickToNotesMap) {
-            val delay = tick - previousTick
-            previousTick = tick
-
-            val sounds = tickNotes.toMap()
-
-            beats.add(SongItemComponent.SoundsWithDelayAfter(sounds, delay.toInt()))
-        }
-
-        return SongItemComponent(beats)
-    }
-
-    override fun toString(): String {
-        return "SongBuilder(startingTick=$startingTick, tickToNotesMap=$tickToNotesMap)"
+        return SongItemComponent(emptyList())
     }
 
 }
