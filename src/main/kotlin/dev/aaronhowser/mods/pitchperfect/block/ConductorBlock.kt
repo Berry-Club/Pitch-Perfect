153ac5f3566e079ff3c25059f1840d43281fa757
Aaron Howser <aaronhowser1@gmail.com>
2024-07-09 14:20:22 -0500

    Shape

--- src/main/kotlin/dev/aaronhowser/mods/pitchperfect/block/ConductorBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/pitchperfect/block/ConductorBlock.kt
@@ -1,8 +1,10 @@
 package dev.aaronhowser.mods.pitchperfect.block
 
 import com.mojang.serialization.MapCodec
+import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
 import net.minecraft.world.item.context.BlockPlaceContext
+import net.minecraft.world.level.BlockGetter
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.HorizontalDirectionalBlock
 import net.minecraft.world.level.block.RenderShape
@@ -10,6 +12,9 @@ import net.minecraft.world.level.block.SoundType
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.block.state.StateDefinition
 import net.minecraft.world.level.material.MapColor
+import net.minecraft.world.phys.shapes.CollisionContext
+import net.minecraft.world.phys.shapes.Shapes
+import net.minecraft.world.phys.shapes.VoxelShape
 
 class ConductorBlock(
     private val properties: Properties = Properties.of()
@@ -36,8 +41,52 @@ class ConductorBlock(
         return RenderShape.MODEL
     }
 
+    override fun getOcclusionShape(pState: BlockState, pLevel: BlockGetter, pPos: BlockPos): VoxelShape {
+        return Boxes.ALL
+    }
+
+    override fun getCollisionShape(
+        pState: BlockState,
+        pLevel: BlockGetter,
+        pPos: BlockPos,
+        pContext: CollisionContext
+    ): VoxelShape {
+        return Boxes.ALL
+    }
+
+    override fun getShape(
+        pState: BlockState,
+        pLevel: BlockGetter,
+        pPos: BlockPos,
+        pContext: CollisionContext
+    ): VoxelShape {
+        return Boxes.ALL
+    }
+
     companion object {
         val CODEC: MapCodec<ConductorBlock> = simpleCodec(::ConductorBlock)
+
+        private object Boxes {
+
+            val STAND: VoxelShape = Block.box(
+                7.0, 1.0, 7.0,
+                7.0 + 2, 1.0 + 15, 7.0 + 2
+            )
+
+            val BASE: VoxelShape = Block.box(
+                4.0, 0.0, 4.0,
+                4.0 + 8, 0.0 + 1, 4.0 + 8
+            )
+
+            val TRAY: VoxelShape = Block.box(
+                3.0, 16.0, 5.0,
+                3.0 + 10, 16.0 + 0.1, 5.0 + 4
+            )
+
+            val ALL: VoxelShape = Shapes.or(STAND, BASE, TRAY)
+
+        }
+
     }
 
     override fun codec(): MapCodec<ConductorBlock> {
