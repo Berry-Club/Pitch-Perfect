ece019b7f279b68da27eb4cd0092e5ffe8c73282
Aaron Howser <aaronhowser1@gmail.com>
2024-07-09 18:16:51 -0500

    register cap

--- src/main/kotlin/dev/aaronhowser/mods/pitchperfect/event/ModBusEvents.kt
+++ src/main/kotlin/dev/aaronhowser/mods/pitchperfect/event/ModBusEvents.kt
@@ -2,8 +2,11 @@ package dev.aaronhowser.mods.pitchperfect.event
 
 import dev.aaronhowser.mods.pitchperfect.PitchPerfect
 import dev.aaronhowser.mods.pitchperfect.packet.ModPacketHandler
+import dev.aaronhowser.mods.pitchperfect.registry.ModBlockEntities
 import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.fml.common.EventBusSubscriber
+import net.neoforged.neoforge.capabilities.Capabilities
+import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent
 
 @EventBusSubscriber(
@@ -15,6 +18,16 @@ object ModBusEvents {
     @SubscribeEvent
     fun registerPayloads(event: RegisterPayloadHandlersEvent) {
         ModPacketHandler.registerPayloads(event)
+    }
+
+    @SubscribeEvent
+    fun onRegisterCapabilities(event: RegisterCapabilitiesEvent) {
+
+        event.registerBlockEntity(
+            Capabilities.ItemHandler.BLOCK,
+            ModBlockEntities.CONDUCTOR.get()
+        ) { blockEntity, direction -> blockEntity.getItemHandler(direction) }
+
     }
 
 }
