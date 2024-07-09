ccd060b022a556aa21ab3ca085ec0252732a2d04
Aaron Howser <aaronhowser1@gmail.com>
2024-07-09 12:57:06 -0500

    Added rotation

--- src/main/kotlin/dev/aaronhowser/mods/pitchperfect/datagen/model/ModBlockStateProvider.kt
+++ src/main/kotlin/dev/aaronhowser/mods/pitchperfect/datagen/model/ModBlockStateProvider.kt
@@ -2,7 +2,9 @@ package dev.aaronhowser.mods.pitchperfect.datagen.model
 
 import dev.aaronhowser.mods.pitchperfect.PitchPerfect
 import dev.aaronhowser.mods.pitchperfect.registry.ModBlocks
+import net.minecraft.core.Direction
 import net.minecraft.data.PackOutput
+import net.minecraft.world.level.block.HorizontalDirectionalBlock
 import net.neoforged.neoforge.client.model.generators.BlockStateProvider
 import net.neoforged.neoforge.client.model.generators.ConfiguredModel
 import net.neoforged.neoforge.client.model.generators.ItemModelBuilder
@@ -23,12 +25,23 @@ class ModBlockStateProvider(
 
         getVariantBuilder(block)
             .forAllStates {
+
+                val facing: Direction = it.getValue(HorizontalDirectionalBlock.FACING)
+                val yRotation = when (facing) {
+                    Direction.NORTH -> 0
+                    Direction.EAST -> 90
+                    Direction.SOUTH -> 180
+                    Direction.WEST -> 270
+                    else -> throw IllegalStateException("Invalid facing direction")
+                }
+
                 ConfiguredModel
                     .builder()
                     .modelFile(
                         models()
                             .getExistingFile(modLoc("block/conductor"))
                     )
+                    .rotationY(yRotation)
                     .build()
             }
 
