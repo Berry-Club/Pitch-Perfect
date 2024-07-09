b07bf52b3ef33c9774523c2e3d8147b48165f41a
Aaron Howser <aaronhowser1@gmail.com>
2024-07-09 11:48:41 -0500

    ConductorBlock

---
+++ src/main/kotlin/dev/aaronhowser/mods/pitchperfect/block/ConductorBlock.kt
@@ -0,0 +1,7 @@
+package dev.aaronhowser.mods.pitchperfect.block
+
+import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.state.BlockBehaviour
+
+class ConductorBlock : Block(Properties.of()) {
+}
---
+++ src/main/kotlin/dev/aaronhowser/mods/pitchperfect/registry/ModBlocks.kt
@@ -0,0 +1,28 @@
+package dev.aaronhowser.mods.pitchperfect.registry
+
+import dev.aaronhowser.mods.pitchperfect.PitchPerfect
+import dev.aaronhowser.mods.pitchperfect.block.ConductorBlock
+import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.state.BlockBehaviour
+import net.neoforged.neoforge.registries.DeferredBlock
+import net.neoforged.neoforge.registries.DeferredRegister
+
+object ModBlocks {
+
+    val BLOCK_REGISTRY: DeferredRegister.Blocks =
+        DeferredRegister.createBlocks(PitchPerfect.ID)
+
+    val CONDUCTOR: DeferredBlock<ConductorBlock> = registerBlock("conductor") { ConductorBlock() }
+
+    private fun <T : Block> registerBlock(
+        name: String,
+        supplier: () -> T = { Block(BlockBehaviour.Properties.of()) as T }
+    ): DeferredBlock<T> {
+        val block = BLOCK_REGISTRY.register(name, supplier)
+
+        ModItems.ITEM_REGISTRY.registerSimpleBlockItem(name, block)
+
+        return block
+    }
+
+}
