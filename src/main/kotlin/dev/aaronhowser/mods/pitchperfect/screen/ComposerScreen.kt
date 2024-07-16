2abb1278b41ded90cd949990c7d0ef2c60387e91
Aaron Howser <aaronhowser1@gmail.com>
2024-07-16 11:33:27 -0500

    Buttons with images!

--- src/main/kotlin/dev/aaronhowser/mods/pitchperfect/screen/ComposerScreen.kt
+++ src/main/kotlin/dev/aaronhowser/mods/pitchperfect/screen/ComposerScreen.kt
@@ -3,18 +3,23 @@ package dev.aaronhowser.mods.pitchperfect.screen
 import dev.aaronhowser.mods.pitchperfect.block.entity.ComposerBlockEntity
 import dev.aaronhowser.mods.pitchperfect.screen.base.ScreenTextures
 import net.minecraft.client.gui.GuiGraphics
+import net.minecraft.client.gui.components.Button
+import net.minecraft.client.gui.components.SpriteIconButton
 import net.minecraft.client.gui.screens.Screen
 import net.minecraft.network.chat.Component
+import net.minecraft.resources.ResourceLocation
 
 class ComposerScreen(
     val composerBlockEntity: ComposerBlockEntity,
     pTitle: Component
 ) : Screen(pTitle) {
 
+    private val buttons: MutableList<Button> = mutableListOf()
+
     override fun renderBackground(pGuiGraphics: GuiGraphics, pMouseX: Int, pMouseY: Int, pPartialTick: Float) {
         this.renderTransparentBackground(pGuiGraphics)
         pGuiGraphics.blit(
-            ScreenTextures.Backgrounds.COMPOSER,
+            ScreenTextures.Background.COMPOSER,
             (width - 512) / 2,
             (height - 256) / 2,
             0f,
@@ -25,5 +30,47 @@ class ComposerScreen(
             512
         )
     }
+
+    override fun init() {
+        addButtons()
+    }
+
+    private fun addButtons() {
+        addSpriteIconButton(0, 0, 20, 20, ScreenTextures.Sprite.Instrument.BANJO)
+        addSpriteIconButton(20, 0, 30, 30, ScreenTextures.Sprite.Instrument.BASS)
+    }
+
+    private fun addSpriteIconButton(
+        x: Int, y: Int,
+        width: Int, height: Int,
+        image: ResourceLocation,
+        component: Component = Component.empty(),
+        onPress: (Button) -> Unit = {}
+    ) {
+        val button = SpriteIconButton
+            .builder(component, onPress, true)
+            .sprite(image, 16, 16)
+            .size(width, height)
+            .build()
+            .apply {
+                this.x = x
+                this.y = y
+            }
+
+        this.buttons.add(button)
+    }
+
+    override fun render(pGuiGraphics: GuiGraphics, pMouseX: Int, pMouseY: Int, pPartialTick: Float) {
+        super.render(pGuiGraphics, pMouseX, pMouseY, pPartialTick)
+
+        renderButtons(pGuiGraphics, pMouseX, pMouseY, pPartialTick)
+    }
+
+    private fun renderButtons(pGuiGraphics: GuiGraphics, pMouseX: Int, pMouseY: Int, pPartialTick: Float) {
+        for (button in buttons) {
+            button.render(pGuiGraphics, pMouseX, pMouseY, pPartialTick)
+        }
+    }
+
 
 }
