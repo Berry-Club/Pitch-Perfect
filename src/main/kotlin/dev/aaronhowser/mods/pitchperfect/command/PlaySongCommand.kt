05ff31ac5d1ecafa792b8d1d555167577c2f0cf6
Aaron Howser <aaronhowser1@gmail.com>
2024-07-15 23:44:21 -0500

    More commands

---
+++ src/main/kotlin/dev/aaronhowser/mods/pitchperfect/command/PlaySongCommand.kt
@@ -0,0 +1,45 @@
+package dev.aaronhowser.mods.pitchperfect.command
+
+import com.mojang.brigadier.builder.ArgumentBuilder
+import com.mojang.brigadier.context.CommandContext
+import dev.aaronhowser.mods.pitchperfect.song.SongPlayer
+import dev.aaronhowser.mods.pitchperfect.song.SongSavedData
+import net.minecraft.commands.CommandSourceStack
+import net.minecraft.commands.Commands
+import net.minecraft.commands.arguments.UuidArgument
+import net.minecraft.server.level.ServerLevel
+import net.minecraft.server.level.ServerPlayer
+
+object PlaySongCommand {
+
+    private const val SONG_ARGUMENT = "song"
+
+    fun register(): ArgumentBuilder<CommandSourceStack, *> {
+        return Commands
+            .literal("playRawSong")
+            .requires { it.hasPermission(2) }
+            .then(
+                Commands
+                    .argument(SONG_ARGUMENT, UuidArgument.uuid())
+                    .executes(::playSong)
+            )
+    }
+
+    private fun playSong(context: CommandContext<CommandSourceStack>): Int {
+        try {
+            val songUuid = UuidArgument.getUuid(context, SONG_ARGUMENT)
+            val player = context.source.entity as? ServerPlayer ?: return 0
+
+            val songSavedData = SongSavedData.get(player)
+            val songInfo = songSavedData.getSongInfo(songUuid) ?: return 0
+
+            val songPlayer = SongPlayer(player.level() as ServerLevel, songInfo.song) { player.eyePosition }
+
+            songPlayer.startPlaying()
+            return 1
+        } catch (e: IllegalArgumentException) {
+            return 0
+        }
+    }
+
+}
---
+++ src/main/kotlin/dev/aaronhowser/mods/pitchperfect/command/RemoveSongCommand.kt
@@ -0,0 +1,37 @@
+package dev.aaronhowser.mods.pitchperfect.command
+
+import com.mojang.brigadier.builder.ArgumentBuilder
+import com.mojang.brigadier.context.CommandContext
+import dev.aaronhowser.mods.pitchperfect.song.SongSavedData
+import net.minecraft.commands.CommandSourceStack
+import net.minecraft.commands.Commands
+import net.minecraft.commands.arguments.UuidArgument
+import net.minecraft.network.chat.Component
+
+object RemoveSongCommand {
+
+    private const val SONG_ARGUMENT = "songUuid"
+
+    fun register(): ArgumentBuilder<CommandSourceStack, *> {
+        return Commands
+            .literal("removeSong")
+            .then(
+                Commands
+                    .argument(SONG_ARGUMENT, UuidArgument.uuid())
+                    .executes(::removeSong)
+            )
+    }
+
+    private fun removeSong(context: CommandContext<CommandSourceStack>): Int {
+        val player = context.source.entity ?: return 0
+        val songSavedData = SongSavedData.get(player)
+
+        val uuid = UuidArgument.getUuid(context, SONG_ARGUMENT)
+        songSavedData.removeSongInfo(uuid)
+
+        player.sendSystemMessage(Component.literal("Song removed"))
+
+        return 1
+    }
+
+}
