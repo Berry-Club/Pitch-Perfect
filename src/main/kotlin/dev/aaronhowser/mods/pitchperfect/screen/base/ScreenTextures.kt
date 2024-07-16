6fc1638a3d2cb10337bd59cb94909c38e2ce5105
Aaron Howser <aaronhowser1@gmail.com>
2024-07-16 11:15:28 -0500

    Added sprites

--- src/main/kotlin/dev/aaronhowser/mods/pitchperfect/screen/base/ScreenTextures.kt
+++ src/main/kotlin/dev/aaronhowser/mods/pitchperfect/screen/base/ScreenTextures.kt
@@ -4,10 +4,34 @@ import dev.aaronhowser.mods.pitchperfect.util.OtherUtil
 
 object ScreenTextures {
 
-    object Backgrounds {
+    object Background {
         val COMPOSER = OtherUtil.modResource("textures/gui/container/composer.png")
 
         const val TEXTURE_SIZE = 256
+    }
+
+    object Sprite {
+
+        object Instrument {
+            val BANJO = OtherUtil.modResource("instrument/banjo")
+            val BASS = OtherUtil.modResource("instrument/bass")
+            val BASS_DRUM = OtherUtil.modResource("instrument/bass_drum")
+            val BELL = OtherUtil.modResource("instrument/bell")
+            val BIT = OtherUtil.modResource("instrument/bit")
+            val CHIME = OtherUtil.modResource("instrument/chime")
+            val COW_BELL = OtherUtil.modResource("instrument/cow_bell")
+            val DIDGERIDOO = OtherUtil.modResource("instrument/didgeridoo")
+            val ELECTRIC_PIANO = OtherUtil.modResource("instrument/electric_piano")
+            val FLUTE = OtherUtil.modResource("instrument/flute")
+            val GLOCKENSPIEL = OtherUtil.modResource("instrument/glockenspiel")
+            val GUITAR = OtherUtil.modResource("instrument/guitar")
+            val HARP = OtherUtil.modResource("instrument/harp")
+            val SNARE_DRUM = OtherUtil.modResource("instrument/snare_drum")
+            val STICKS = OtherUtil.modResource("instrument/sticks")
+            val VIBRAPHONE = OtherUtil.modResource("instrument/vibraphone")
+            val XYLOPHONE = OtherUtil.modResource("instrument/xylophone")
+        }
+
     }
 
 }
