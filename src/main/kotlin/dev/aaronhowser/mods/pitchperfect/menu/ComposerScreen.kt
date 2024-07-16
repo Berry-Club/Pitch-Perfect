54d972c0191587a01e6a0ca9060d8b3e4bca282a
Aaron Howser <aaronhowser1@gmail.com>
2024-07-16 04:29:09 -0500

    Progress

--- src/main/kotlin/dev/aaronhowser/mods/pitchperfect/menu/ComposerScreen.kt
+++ src/main/kotlin/dev/aaronhowser/mods/pitchperfect/menu/ComposerScreen.kt
@@ -1,10 +1,8 @@
 package dev.aaronhowser.mods.pitchperfect.menu
 
-import com.mojang.blaze3d.systems.RenderSystem
 import dev.aaronhowser.mods.pitchperfect.menu.base.ScreenTextures
 import net.minecraft.client.gui.GuiGraphics
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen
-import net.minecraft.client.renderer.GameRenderer
 import net.minecraft.network.chat.Component
 import net.minecraft.world.entity.player.Inventory
 
@@ -15,9 +13,6 @@ class ComposerScreen(
 ) : AbstractContainerScreen<ComposerMenu>(pMenu, pPlayerInventory, pTitle) {
 
     override fun renderBg(pGuiGraphics: GuiGraphics, pPartialTick: Float, pMouseX: Int, pMouseY: Int) {
-        RenderSystem.setShader { GameRenderer.getPositionTexShader() }
-        RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f)
-        RenderSystem.setShaderTexture(0, ScreenTextures.Backgrounds.COMPOSER)
 
         val x = (width - imageWidth) / 2
         val y = (height - imageHeight) / 2
@@ -31,6 +26,12 @@ class ComposerScreen(
             ScreenTextures.Backgrounds.TEXTURE_SIZE,
             ScreenTextures.Backgrounds.TEXTURE_SIZE
         )
+    }
+
+    override fun render(pGuiGraphics: GuiGraphics, pMouseX: Int, pMouseY: Int, pPartialTick: Float) {
+        renderBackground(pGuiGraphics, pMouseX, pMouseY, pPartialTick)
+        super.render(pGuiGraphics, pMouseX, pMouseY, pPartialTick)
+        renderTooltip(pGuiGraphics, pMouseX, pMouseY)
     }
 
 }
