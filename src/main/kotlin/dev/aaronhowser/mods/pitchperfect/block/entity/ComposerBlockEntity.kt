d0ad2fc0606a8c87c45882e67900bddcf9e89992
Aaron Howser <aaronhowser1@gmail.com>
2024-07-21 17:05:37 -0500

    getUpdateTag and getUpdatePacket

--- src/main/kotlin/dev/aaronhowser/mods/pitchperfect/block/entity/ComposerBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/pitchperfect/block/entity/ComposerBlockEntity.kt
@@ -7,6 +7,9 @@ import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
 import net.minecraft.core.HolderLookup
 import net.minecraft.nbt.CompoundTag
+import net.minecraft.network.protocol.Packet
+import net.minecraft.network.protocol.game.ClientGamePacketListener
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket
 import net.minecraft.world.Containers
 import net.minecraft.world.SimpleContainer
 import net.minecraft.world.entity.player.Player
@@ -75,6 +78,14 @@ class ComposerBlockEntity(
             pTag.put(TIMELINE, timeline.toTag())
             println("Saved composer timeline: $timeline with soundcounts:\n${timeline.soundCounts}")
         }
+    }
+
+    override fun getUpdateTag(pRegistries: HolderLookup.Provider): CompoundTag {
+        return saveWithoutMetadata(pRegistries)
+    }
+
+    override fun getUpdatePacket(): Packet<ClientGamePacketListener>? {
+        return ClientboundBlockEntityDataPacket.create(this)
     }
 
     fun clickCell(
