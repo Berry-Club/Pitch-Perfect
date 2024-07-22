55feef90976b8c58dc0d882de0c9f7e8df3a01d6
Aaron Howser <aaronhowser1@gmail.com>
2024-07-21 22:06:19 -0500

    codecs

--- src/main/kotlin/dev/aaronhowser/mods/pitchperfect/song/parts/SongWip.kt
+++ src/main/kotlin/dev/aaronhowser/mods/pitchperfect/song/parts/SongWip.kt
@@ -1,11 +1,21 @@
 package dev.aaronhowser.mods.pitchperfect.song.parts
 
+import com.mojang.serialization.Codec
 import net.minecraft.core.Holder
+import net.minecraft.network.RegistryFriendlyByteBuf
+import net.minecraft.network.codec.StreamCodec
 import net.minecraft.sounds.SoundEvent
 
 class SongWip(
     private var song: Song
 ) {
+
+    companion object {
+        val CODEC: Codec<SongWip> =
+            Song.CODEC.xmap(::SongWip, SongWip::song)
+        val STREAM_CODEC: StreamCodec<RegistryFriendlyByteBuf, SongWip> =
+            Song.STREAM_CODEC.map(::SongWip, SongWip::song)
+    }
 
     constructor() : this(Song(emptyMap()))
 
