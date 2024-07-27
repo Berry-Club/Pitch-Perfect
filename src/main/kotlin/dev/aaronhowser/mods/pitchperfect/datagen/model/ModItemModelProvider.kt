f9c6274cf77c6cd510aa673c2f584a1dd6e52831
Aaron Howser <aaronhowser1@gmail.com>
2024-07-27 00:34:28 -0500

    made digeridoo longer

--- src/main/kotlin/dev/aaronhowser/mods/pitchperfect/datagen/model/ModItemModelProvider.kt
+++ src/main/kotlin/dev/aaronhowser/mods/pitchperfect/datagen/model/ModItemModelProvider.kt
@@ -4,7 +4,9 @@ import dev.aaronhowser.mods.pitchperfect.PitchPerfect
 import dev.aaronhowser.mods.pitchperfect.registry.ModItems
 import net.minecraft.data.PackOutput
 import net.minecraft.world.item.BlockItem
+import net.minecraft.world.item.ItemDisplayContext
 import net.neoforged.neoforge.client.model.generators.ItemModelProvider
+import net.neoforged.neoforge.client.model.generators.ModelFile
 import net.neoforged.neoforge.common.data.ExistingFileHelper
 
 class ModItemModelProvider(
@@ -12,14 +14,46 @@ class ModItemModelProvider(
     existingFileHelper: ExistingFileHelper
 ) : ItemModelProvider(output, PitchPerfect.ID, existingFileHelper) {
 
-    //TODO: Make Didgeridoo bigger
     override fun registerModels() {
 
         for (item in ModItems.ITEM_REGISTRY.entries) {
             if (item.get() is BlockItem) continue
+
+            if (item === ModItems.DIDGERIDOO) {
+                didgeridoo()
+                continue
+            }
+
             basicItem(item.get())
         }
 
+    }
+
+    private fun didgeridoo() {
+        val didgeridooItem = ModItems.DIDGERIDOO.get()
+
+        getBuilder(didgeridooItem.toString())
+            .parent(ModelFile.UncheckedModelFile("item/generated"))
+            .texture("layer0", "item/didgeridoo")
+            .transforms()
+
+            .transform(ItemDisplayContext.FIRST_PERSON_LEFT_HAND)
+            .scale(1.25f)
+            .end()
+
+            .transform(ItemDisplayContext.FIRST_PERSON_RIGHT_HAND)
+            .scale(1.25f)
+            .end()
+
+            .transform(ItemDisplayContext.THIRD_PERSON_LEFT_HAND)
+            .scale(1.25f)
+            .end()
+
+            .transform(ItemDisplayContext.THIRD_PERSON_RIGHT_HAND)
+            .scale(1.25f)
+            .end()
+
+            .end()
     }
 
 }
