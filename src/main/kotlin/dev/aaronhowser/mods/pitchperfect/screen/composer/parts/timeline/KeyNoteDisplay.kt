8420dc8ed85d73e5c1f45fe2cbda3c84b6d5bea3
Aaron Howser <aaronhowser1@gmail.com>
2024-07-27 01:36:28 -0500

    TODO

---
+++ src/main/kotlin/dev/aaronhowser/mods/pitchperfect/screen/composer/parts/timeline/KeyNoteDisplay.kt
@@ -0,0 +1,7 @@
+package dev.aaronhowser.mods.pitchperfect.screen.composer.parts.timeline
+
+class KeyNoteDisplay {
+
+    // TODO: Double-wide cells that look like piano keys, white and black for the appropriate notes, like FL Studio
+
+}
