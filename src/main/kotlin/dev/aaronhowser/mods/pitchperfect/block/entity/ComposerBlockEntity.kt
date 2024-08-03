84d3041434306166d048a47287b76abde94b4905
Aaron Howser <aaronhowser1@gmail.com>
2024-08-03 09:05:18 -0500

    Revert "removed the component builder thing"
    This reverts commit 3fb9faa5f321ae89821dbd89600d49030da679cc.

--- src/main/kotlin/dev/aaronhowser/mods/pitchperfect/block/entity/ComposerBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/pitchperfect/block/entity/ComposerBlockEntity.kt
@@ -1,6 +1,8 @@
 package dev.aaronhowser.mods.pitchperfect.block.entity
 
+import dev.aaronhowser.mods.pitchperfect.item.component.SongComponent
 import dev.aaronhowser.mods.pitchperfect.registry.ModBlockEntities
+import dev.aaronhowser.mods.pitchperfect.registry.ModDataComponents
 import dev.aaronhowser.mods.pitchperfect.song.parts.Author
 import dev.aaronhowser.mods.pitchperfect.song.parts.Note
 import dev.aaronhowser.mods.pitchperfect.song.parts.Song
@@ -8,6 +10,7 @@ import dev.aaronhowser.mods.pitchperfect.song.parts.SongWip
 import dev.aaronhowser.mods.pitchperfect.util.OtherUtil.getUuidOrNull
 import net.minecraft.core.BlockPos
 import net.minecraft.core.HolderLookup
+import net.minecraft.core.component.DataComponentMap
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.nbt.ListTag
 import net.minecraft.network.protocol.Packet
@@ -85,6 +88,14 @@ class ComposerBlockEntity(
         super.setChanged()
 
         level?.sendBlockUpdated(blockPos, blockState, blockState, 1 or 2 or 8)
+
+        val song = songWip?.song
+        if (song != null) {
+            val componentBuilder = DataComponentMap.builder()
+            componentBuilder.set(ModDataComponents.SONG_WIP_COMPONENT, SongComponent(song))
+
+            this.setComponents(componentBuilder.build())
+        }
     }
 
     fun clickCell(
