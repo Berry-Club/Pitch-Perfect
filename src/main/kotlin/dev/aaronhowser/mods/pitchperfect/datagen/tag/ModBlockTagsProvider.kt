e2c6fe960b88cacbf7b6f15ddd83a04d88faa0d9
Aaron Howser <aaronhowser1@gmail.com>
2024-08-03 07:56:19 -0500

    block mine tag

--- src/main/kotlin/dev/aaronhowser/mods/pitchperfect/datagen/tag/ModBlockTagsProvider.kt
+++ src/main/kotlin/dev/aaronhowser/mods/pitchperfect/datagen/tag/ModBlockTagsProvider.kt
@@ -1,8 +1,10 @@
 package dev.aaronhowser.mods.pitchperfect.datagen.tag
 
 import dev.aaronhowser.mods.pitchperfect.PitchPerfect
+import dev.aaronhowser.mods.pitchperfect.registry.ModBlocks
 import net.minecraft.core.HolderLookup
 import net.minecraft.data.PackOutput
+import net.minecraft.tags.BlockTags
 import net.neoforged.neoforge.common.data.BlockTagsProvider
 import net.neoforged.neoforge.common.data.ExistingFileHelper
 import java.util.concurrent.CompletableFuture
@@ -14,6 +16,9 @@ class ModBlockTagsProvider(
 ) : BlockTagsProvider(output, lookupProvider, PitchPerfect.ID, existingFileHelper) {
 
     override fun addTags(pProvider: HolderLookup.Provider) {
-        // Why is this required for item tags???
+
+        tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModBlocks.CONDUCTOR.get())
+        tag(BlockTags.MINEABLE_WITH_AXE).add(ModBlocks.COMPOSER.get())
+
     }
 }
